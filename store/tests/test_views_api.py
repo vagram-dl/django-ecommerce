25f0eb6fe3de4dd6d6157788db25d76e918ed1e6
Katranyan Vagram <vagram.katr@gmail.com>
2026-02-15 21:54:12 +0300

    Add API tests for product creation

--- store/tests/test_views_api.py
+++ store/tests/test_views_api.py
@@ -82,4 +82,40 @@ class ProductViewSetTestCase(APITestCase):
 
         self.assertEqual(response.data['count'],2)
 
+    def test_retrieve_product_detail(self):
+        url = reverse('product-detail',args=[self.product1.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data['name'],'Green Tea')
+        self.assertEqual(response.data['price'],'259.99')
+        self.assertEqual(response.data['category'],self.category_tea.id)
+
+    def test_retrieve_product_not_found(self):
+        url = reverse('product-detail',args=[999])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+
+    def test_create_product_as_admin(self):
+        self.client.force_authenticate(user=self.admin_user)
+
+        new_product = {
+            'name' : 'Oolong Tea',
+            'price':'350.00',
+            'category':self.category_tea.id,
+            'description':'Traditional Chinese tea'
+        }
+
+        response = self.client.post(self.products_url,new_product,format='json')
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(),3)
+
+        new_product_db = Product.objects.get(name='Oolong Tea')
+        self.assertEqual(new_product_db.price,350.00)
+        self.assertEqual(new_product_db.category,self.category_tea)
+        self.assertEqual(new_product_db.description, 'Traditional Chinese tea')
+
+        self.assertEqual(response.data['name'],'Oolong Tea')
+        self.assertEqual(response.data['price'],'350.00')
 
